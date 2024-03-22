4193e745fbcb3b0251075c64f7109181c206a1d4
Marco Rinaldi <marcorinaldi519@gmail.com>
2024-03-22 17:39:00 -0300

    Repositorio Jogos

--- src/main/kotlin/com/marcorinaldi/jogos/Main.kt
+++ src/main/kotlin/com/marcorinaldi/jogos/Main.kt
@@ -1,14 +1,13 @@
 package com.marcorinaldi.jogos
 
-import com.marcorinaldi.jogos.dados.repositorio.RepositorioJogosImpl
-import com.marcorinaldi.jogos.dominio.repositorio.RepositorioJogos
+import com.marcorinaldi.jogos.dependencias.InjecaoDependencias
 
 fun main() {
     println("### JOGOS MARCO ###")
     print("Digite o ID do jogo: ")
 
     val id = readln()
-    val repositorio: RepositorioJogos = RepositorioJogosImpl()
+    val repositorio = InjecaoDependencias.repositorioJogos()
     val jogo = repositorio.buscarJogo(id)
 
     println(jogo)
--- src/main/kotlin/com/marcorinaldi/jogos/dependencias/InjecaoDependencias.kt
+++ src/main/kotlin/com/marcorinaldi/jogos/dependencias/InjecaoDependencias.kt
@@ -2,10 +2,16 @@ package com.marcorinaldi.jogos.dependencias
 
 import com.marcorinaldi.jogos.dados.api.ApiJogos
 import com.marcorinaldi.jogos.dados.api.ApiJogosImpl
+import com.marcorinaldi.jogos.dados.repositorio.RepositorioJogosImpl
+import com.marcorinaldi.jogos.dominio.repositorio.RepositorioJogos
 
 object InjecaoDependencias {
 
     fun apiJogos(): ApiJogos {
         return ApiJogosImpl()
+    }
+
+    fun repositorioJogos(): RepositorioJogos {
+        return RepositorioJogosImpl()
     }
 }
